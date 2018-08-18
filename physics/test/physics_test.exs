ceaab731cf52af29e8a42378c4c2442da3ce6a32
Christopher Choi <cdddchris@gmail.com>
2018-08-18 00:37:58 -0700

    add assertions, will fill this out more..

--- physics/test/physics_test.exs
+++ physics/test/physics_test.exs
@@ -5,12 +5,12 @@ defmodule PhysicsTest do
   test "escape velocity of earth is correct" do
     #notice how you can pipe directly to IO.inspect? And still get the value returned...
     ev = Physics.Rocketry.escape_velocity(:earth) |> IO.inspect
-    assert ev == 11.2 #what should this value be?
+    assert ev == 11.2 
   end
 
   test "escape velocity of planet X is correct" do
     planet_x = %{mass: 4.0e22, radius: 6.21e6}
     ev =  planet_x |> Physics.Rocketry.escape_velocity |> IO.inspect
-    #use assert here
+    assert ev == 1.0 
   end
 end
