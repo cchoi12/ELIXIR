38f1456dd6b6209cb2dc5f51d6b0a5ad8dba176a
Christopher Choi <cdddchris@gmail.com>
2018-08-24 00:31:49 -0700

    Move converter to its own module and import it, refactor variables

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -1,89 +1,23 @@
-defmodule ConverterTwo do
-
-  def to_nearest_tenth(val) do
-    round_to(val, 1)
-  end
-
-  def to_km(val) do
-    val / 1000
-  end
-
-  def to_meters(val) do
-    val * 1000
-  end
-
-  def to_light_seconds(arg), do: to_light_seconds(arg, precision: 5)
-
-  def to_light_seconds({unit, val}, precision: precision) do
-    light_seconds = case unit do
-      :miles  -> from_miles(val)
-      :meters -> from_meters(val)
-      :feet   -> from_feet(val)
-      :inches -> from_inches(val)
-    end
-    light_seconds |> round_to(precision)
-  end
-
-  defp from_miles(val), do: val * 5.36819e-6
-  defp from_meters(val), do: val * 3.335638620368e-9
-  defp from_feet(val), do: val * 1.016702651488166404e-9
-  defp from_inches(val), do: val * 8.472522095734715723e-11
-
-
-  defp round_to(val, precision) when is_float(val) do
-    Float.round(val, precision)
-  end
-
-end
-
-defmodule Converter do
-
-  def to_nearest_tenth(val) do
-    round_to(val, 1)
-  end
-
-  def to_km(val) do
-    val / 1000
-  end
-
-  def to_meters(val) do
-    val * 1000
-  end
-
-  def to_light_seconds({unit, val}, precision: precision) do
-    case unit do
-      :miles  -> from_miles(val)
-      :meters -> from_meters(val)
-      :feet   -> from_feet(val)
-      :inches -> from_inches(val)
-    end |> round_to(precision)
-  end
-
-  defp from_miles(val), do: val * 5.36819e-6
-  defp from_meters(val), do: val * 3.335638620368e-9
-  defp from_feet(val), do: val * 1.016702651488166404e-9
-  defp from_inches(val), do: val * 8.472522095734715723e-11
-
-  defp round_to(val, precision) when is_float(val) do
-    Float.round(val, precision)
-  end
-
-end
-
 defmodule Physics.Rocketry do
+  import Converter
+
+  @moon %{mass: 7.35e22, radius: 1.738e6}
+  @mars %{mass: 6.39e23, radius: 3.4e6}
+  @earth %{mass: 5.972e24, radius: 6.371e6}
+  @newtons_constant 6.67e-11
 
   def escape_velocity(:earth) do
-    %{mass: 5.972e24, radius: 6.371e6}
+    @earth
       |> escape_velocity
   end
 
   def escape_velocity(:mars) do
-    %{mass: 6.39e23, radius: 3.4e6}
+    @mars
       |> escape_velocity
   end
 
   def escape_velocity(:moon) do
-    %{mass: 7.35e22, radius: 1.738e6}
+    @moon
       |> escape_velocity
   end
 
@@ -95,8 +29,7 @@ defmodule Physics.Rocketry do
   end
 
   defp calculate_escape(%{mass: mass, radius: radius}) do
-    newtons_constant = 6.67e-11
-    2 * newtons_constant * mass / radius
+    2 * @newtons_constant * mass / radius
       |> :math.sqrt
   end
 
