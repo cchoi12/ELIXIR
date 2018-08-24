3dc5dedd779dbef920b356d2c4c50a1c99435b6d
Christopher Choi <cdddchris@gmail.com>
2018-08-24 14:39:38 -0700

    Move converters around

--- physics/lib/converter.ex
+++ physics/lib/converter.ex
@@ -1,3 +1,37 @@
+defmodule Converter do
+
+  def to_nearest_tenth(val) do
+    round_to(val, 1)
+  end
+
+  def to_km(val) do
+    val / 1000
+  end
+
+  def to_meters(val) do
+    val * 1000
+  end
+
+  def to_light_seconds({unit, val}, precision: precision) do
+    case unit do
+      :miles  -> from_miles(val)
+      :meters -> from_meters(val)
+      :feet   -> from_feet(val)
+      :inches -> from_inches(val)
+    end |> round_to(precision)
+  end
+
+  defp from_miles(val), do: val * 5.36819e-6
+  defp from_meters(val), do: val * 3.335638620368e-9
+  defp from_feet(val), do: val * 1.016702651488166404e-9
+  defp from_inches(val), do: val * 8.472522095734715723e-11
+
+  defp round_to(val, precision) when is_float(val) do
+    Float.round(val, precision)
+  end
+
+end
+
 defmodule ConverterTwo do
 
   def to_nearest_tenth(val) do
@@ -29,40 +63,6 @@ defmodule ConverterTwo do
   defp from_feet(val), do: val * 1.016702651488166404e-9
   defp from_inches(val), do: val * 8.472522095734715723e-11
 
-
-  defp round_to(val, precision) when is_float(val) do
-    Float.round(val, precision)
-  end
-
-end
-
-defmodule Converter do
-
-  def to_nearest_tenth(val) do
-    round_to(val, 1)
-  end
-
-  def to_km(val) do
-    val / 1000
-  end
-
-  def to_meters(val) do
-    val * 1000
-  end
-
-  def to_light_seconds({unit, val}, precision: precision) do
-    case unit do
-      :miles  -> from_miles(val)
-      :meters -> from_meters(val)
-      :feet   -> from_feet(val)
-      :inches -> from_inches(val)
-    end |> round_to(precision)
-  end
-
-  defp from_miles(val), do: val * 5.36819e-6
-  defp from_meters(val), do: val * 3.335638620368e-9
-  defp from_feet(val), do: val * 1.016702651488166404e-9
-  defp from_inches(val), do: val * 8.472522095734715723e-11
 
   defp round_to(val, precision) when is_float(val) do
     Float.round(val, precision)
