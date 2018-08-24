4bafb656818aeb3753e980362e62f0516e9b63c9
Christopher Choi <cdddchris@gmail.com>
2018-08-24 00:44:57 -0700

    Remove unneeded code and use import instead

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -1,25 +1,23 @@
 defmodule Physics.Rocketry do
   import Converter
+  import Calcs
+  import Physics.Laws
+  import Planets
 
-  @moon %{mass: 7.35e22, radius: 1.738e6}
-  @mars %{mass: 6.39e23, radius: 3.4e6}
-  @earth %{mass: 5.972e24, radius: 6.371e6}
-  @newtons_constant 6.67e-11
-
-  def escape_velocity(:earth), do: @earth |> escape_velocity
-  def escape_velocity(:mars), do: @mars |> escape_velocity
-  def escape_velocity(:moon), do: @moon |> escape_velocity
+  def escape_velocity(:earth), do: earth |> escape_velocity
+  def escape_velocity(:mars), do: mars |> escape_velocity
+  def escape_velocity(:moon), do: moon |> escape_velocity
 
   def escape_velocity(planet) when is_map(planet) do
     planet
       |> calculate_escape
-      |> Converter.to_km
-      |> Converter.to_nearest_tenth
+      |> to_km
+      |> to_nearest_tenth
   end
 
   defp calculate_escape(%{mass: mass, radius: radius}) do
-    2 * @newtons_constant * mass / radius
-      |> :math.sqrt
+    2 * newtons_gravitational_constant * mass / radius
+      |> square_root
   end
 
 end
