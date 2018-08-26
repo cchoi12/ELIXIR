00a2458cedbc9b8f349776f445d5ae512ecbe23a
Christopher Choi <cdddchris@gmail.com>
2018-08-25 17:38:06 -0700

    add () for mappings, was getting a warning regarding it

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -4,9 +4,9 @@ defmodule Physics.Rocketry do
   import Physics.Laws
   import Planets
 
-  def escape_velocity(:earth), do: earth |> escape_velocity
-  def escape_velocity(:mars), do: mars |> escape_velocity
-  def escape_velocity(:moon), do: moon |> escape_velocity
+  def escape_velocity(:earth), do: earth() |> escape_velocity
+  def escape_velocity(:mars), do: mars() |> escape_velocity
+  def escape_velocity(:moon), do: moon() |> escape_velocity
 
   def escape_velocity(planet) when is_map(planet) do
     planet
@@ -15,9 +15,21 @@ defmodule Physics.Rocketry do
       |> to_nearest_tenth
   end
 
-  defp calculate_escape(%{mass: mass, radius: radius}) do
-    2 * newtons_gravitational_constant * mass / radius
+  def orbital_acceleration(height) do
+    (orbital_speed(height) |> squared) / orbital_radius(height)
+  end
+
+  def orbital_speed(height) do
+    newtons_gravitational_constant() * Planets.earth().mass / orbital_radius(height)
       |> square_root
   end
 
+  defp calculate_escape(%{mass: mass, radius: radius}) do
+    2 * newtons_gravitational_constant() * mass / radius
+      |> square_root
+  end
+
+  defp orbital_radius(height) do
+    Planets.earth().radius + (height |> to_meters)
+  end
 end
