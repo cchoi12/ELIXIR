2a6026514c090230c9e493cc6a596ac72b900045
Christopher Choi <cdddchris@gmail.com>
2018-09-08 12:36:22 -0700

    Add tests for module methods

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,17 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "creates deck successfully" do
+    assert ["Ace", "Two", "Three"] == Cards.create_deck()
+  end
+
+  test "shuffle cards randomizes" do
+    deck = Cards.create_deck()
+    refute deck == Cards.shuffle(deck)
+  end
+
+  test "contains detects match successfully" do
+    deck = ["Ace", "Two", "Three"]
+    assert Cards.contains?(deck, "Ace") == true
   end
 end
