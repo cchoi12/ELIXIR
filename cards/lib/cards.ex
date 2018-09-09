3b4ab88c3d15bc19004d663bc3094f5f3d0f5170
Christopher Choi <cdddchris@gmail.com>
2018-09-08 21:55:55 -0700

    Add additional functionality

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,6 +1,12 @@
 defmodule Cards do
   import Values
 
+  def create_hand do
+    Cards.create_deck()
+    |> shuffle
+    |> deal(5)
+  end
+
   def create_deck do
     for suit <- Values.suits(), value <- Values.values(), uniq: true do
       "#{value} of #{suit}"
@@ -17,5 +23,17 @@ defmodule Cards do
 
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
   end
 end
