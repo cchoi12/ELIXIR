7890a575adbbbdcc1610dadaf124ed0431ddf2b4
Christopher Choi <cdddchris@gmail.com>
2018-09-08 22:26:11 -0700

    Add in exdoc

--- cards/mix.exs
+++ cards/mix.exs
@@ -6,7 +6,7 @@ defmodule Cards.Mixfile do
       app: :cards,
       version: "0.1.0",
       elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
@@ -23,6 +23,7 @@ defmodule Cards.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.19", only: :dev}
     ]
   end
 end
