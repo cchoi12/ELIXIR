c792619b0b323e45f3049bb897254dd7592799c7
Christopher Choi <cdddchris@gmail.com>
2018-10-05 22:57:04 +0900

    Update identicon project

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -12,13 +12,26 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
-    |> build_pixel_map
-    |> draw_image
+
+    #  |> build_pixel_map
+
+    # |> draw_image
   end
 
-  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
-  end
+  # def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+  # end
 
+  @doc """
+  Builds tuple pairs for plots on the image builder
+
+  ## Example
+      iex> filtered_grid = Identicon.hash_input("test") |> Identicon.build_grid |> Identicon.filter_odd_squares
+      iex> filtered_grid |> Identiconbuild_pixel_map
+  %Identicon.Image{color: {9, 143, 107}, grid: [{70, 6}, {70, 8}, {202, 12}, {222, 15}, {78, 16}, {78, 18}, {222, 19}, {38, 20}, {180, 22}, {38, 24}],
+  hex: [9, 143, 107, 205, 70, 33, 211, 115, 202, 222, 78, 131, 38, 39, 180, 246],
+  pixel_map: [{{50, 50}, {100, 100}}, {{150, 50}, {200, 100}}, {{100, 100}, {150, 150}}, {{0, 150}, {50, 200}}, {{50, 150}, {100, 200}}, {{150, 150}, {200, 200}}, {{200, 150}, {250, 200}}, {{0, 200}, {50, 250}}, {{100, 200}, {150, 250}}, {{200, 200}, {250, 250}}]}
+
+  """
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
     pixel_map =
       Enum.map(grid, fn {_code, index} ->
@@ -60,8 +73,8 @@ defmodule Identicon do
 
   ## Example
       iex> hex = Identicon.hash_input("test")
-      iex> graph = Identicon.build_grid(hex)
-      iex> graph
+      iex> grid = Identicon.build_grid(hex)
+      iex> grid
       %Identicon.Image{
       color: nil,
       grid: [{9, 0}, {143, 1}, {107, 2}, {143, 3}, {9, 4}, {205, 5}, {70, 6}, {33, 7}, {70, 8}, {205, 9}, {211, 10}, {115, 11}, {202, 12}, {115, 13}, {211, 14}, {222, 15}, {78, 16}, {131, 17}, {78, 18}, {222, 19}, {38, 20}, {39, 21}, {180, 22}, {39, 23}, {38, 24}],
